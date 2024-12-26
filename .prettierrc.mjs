be432233bd864c2daa769293e496f8928576e5df
flyingCrp <alexchen1875@gmail.com>
2024-12-26 17:34:36 +0800

    refactor(initial project):

---
+++ .prettierrc.mjs
@@ -0,0 +1,6 @@
+module.exports = {
+    tabWidth: 4,
+    useTabs: false,
+    endOfLine: "lf",
+    semi: true
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
 export default function Home() {
     return (
-        <div>
-            llm-function-calling
-        </div>
+        <div> llm-function-calling </div>
     );
 }
